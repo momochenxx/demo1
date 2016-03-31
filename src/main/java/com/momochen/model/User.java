9d9749f7b0d5dfef4b830394bc32d717d555b3d8
momochen <silence_hull@sina.com>
2016-03-31 16:35:36 +0800

    修改

---
+++ src/main/java/com/momochen/model/User.java
@@ -0,0 +1,34 @@
+package com.momochen.model;
+
+/**
+ * Created by momochen on 2016/3/31.
+ */
+public class User {
+    private int id;
+    private String uname;
+    private String upass;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUpass() {
+        return upass;
+    }
+
+    public void setUpass(String upass) {
+        this.upass = upass;
+    }
+}
